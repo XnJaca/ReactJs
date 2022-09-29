c9bc0788c2786eb513276e64c048b55f817c4e73
XanJaca <jocrayzu@gmail.com>
2022-09-29 10:24:18 -0600

    Update Counter App

--- 02-counter-app-vite/src/components/CounterApp.jsx
+++ 02-counter-app-vite/src/components/CounterApp.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useState } from "react";
 
+export const CounterApp = ({ value }) => {
 
-const CounterApp = ({value}) => {
-    return (
-        <div>
-            <h1>CounterApp</h1>
-            <h2> {value}</h2>
-        </div>
-    );
+  const [ counter, setCounter ] = useState( value );
+
+  const addNumber = () => {
+    setCounter(counter + 1);
+  };
+
+  const substract = () => {
+    setCounter(counter - 1);
+  }
+
+  const restore = () =>{
+    setCounter(value);
+  }
+
+  return (
+    <div>
+      <h1>CounterApp</h1>
+      <h2> {counter} </h2>
+
+      <button onClick={substract}> -1 </button>
+      <button onClick={restore}> 0 </button>
+      <button onClick={addNumber}> +1 </button>
+    </div>
+  );
 };
-
 
 CounterApp.propTypes = {
-
+  value: PropTypes.number,
 };
-
-
-export default CounterApp;
--- 02-counter-app-vite/src/main.jsx
+++ 02-counter-app-vite/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { HelloWorldApp } from './HelloWorldApp';
+import {CounterApp} from './components/CounterApp';
 // import 'bootstrap/dist/css/bootstrap.css';
 import './assets/styles.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp name='XanJaca'></FirstApp>
+        <CounterApp value={123}></CounterApp>
     </React.StrictMode>
 )
